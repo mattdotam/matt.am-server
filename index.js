0ed709d92e8b88023f3b86b65b1b4e0b159714d0
Matt Bear <hi@matt.am>
2019-08-26 01:17:33 +1000

    modify index to serve build of client react app

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
 const env = process.env["env"] || "development";
 
+if (env === "production") {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, "client", "build", "index.html")
+		);
+	});
+}
+
 app.get("/", (req, res) => {
-	res.send({ env: env, test: "8" });
+	res.send({
+		hi: "there",
+	});
 });
 
 app.listen(1337);
