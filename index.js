1987265de8a54cdc170b9b55f7f2444454a11d19
Matt Bear <hi@matt.am>
2019-08-25 21:38:11 +1000

    environment variable test #7

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const env = process.env["env"] || "development";
 
 app.get("/", (req, res) => {
-	res.send({ env: env });
+	res.send({ env: env, test: "7" });
 });
 
 app.listen(1337);
