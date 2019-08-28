b61d19d1cbe1c934e53bc73f2cb05008d3cde0c2
Matt Bear <hi@matt.am>
2019-08-28 16:37:27 +1000

    change catch all get requests to follow documentation example

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ require("./routes/trackRoutes")(app);
 
 if (env.env === "production") {
 	const root = path.join(__dirname, "client", "build");
-	app.use("/", express.static(root));
-	app.get("*", (req, res) => {
+	app.use(express.static(root));
+	app.get("/*", (req, res) => {
 		res.sendFile("index.html", { root });
 	});
 }
