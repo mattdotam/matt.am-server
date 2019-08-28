bda246c462db57ff156a2b6a07df4eee9d27b05e
Matt Bear <hi@matt.am>
2019-08-28 12:04:05 +1000

    adding slash to routing catchall to capture route paramaters

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ require("./routes/trackRoutes")(app);
 if (env.env === "production") {
 	const root = require("path").join(__dirname, "client", "build");
 	app.use(express.static(root));
-	app.get("*", (req, res) => {
+	app.get("/*", (req, res) => {
 		res.sendFile("index.html", { root });
 	});
 }
