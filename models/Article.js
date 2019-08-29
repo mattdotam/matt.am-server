11e8a3cb39d2a1490c24b31a676ba2744fe56fb3
Matt Bear <hi@matt.am>
2019-08-29 12:13:01 +1000

    create article collection schema

--- models/Article.js
+++ models/Article.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const coursenoteSchema = new Schema({
+const articleSchema = new Schema({
 	title: String,
 	slug: String,
 	tags: [{ type: String }],
 	content: String,
-	courselink: String,
-	lecturer: String,
 	published: Number,
 	lastEdit: Number,
 });
 
-mongoose.model("coursenote", coursenoteSchema);
+mongoose.model("article", articleSchema);
