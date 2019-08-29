30a45b6cdd31dd1384a1bda6f1a89e3e3d08ab26
Matt Bear <hi@matt.am>
2019-08-29 14:16:02 +1000

    logging track api response data

--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -5,6 +5,7 @@ module.exports = app => {
 	app.get("/api/track/:date", (req, res) => {
 		console.log(req.params.date);
 		Commit.find({}).then(data => {
+			console.log(data);
 			res.send(data);
 		});
 	});
