8ad9c1d50c7d48bf023e18cc677468420bad2e5d
Matt Bear <hi@matt.am>
2019-08-29 20:41:23 +1000

    add param values to commit collection queries and return single day lookups only

--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -2,8 +2,13 @@ const mongoose = require("mongoose");
 const Commit = mongoose.model("commit");
 
 module.exports = app => {
-	app.get("/api/track/", (req, res) => {
-		Commit.find({}).then(data => {
+	app.get("/api/track/:date", (req, res) => {
+		Commit.find({
+			timestamp: {
+				$gte: req.params.date,
+				$lte: Number(req.params.date) + 86399,
+			},
+		}).then(data => {
 			res.send(data);
 		});
 	});
