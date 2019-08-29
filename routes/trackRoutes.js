c1206ceba7141c3466afb560dc3ea481fb6505fc
Matt Bear <hi@matt.am>
2019-08-29 14:43:14 +1000

    change base track api get method to return all commits

--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 const Commit = mongoose.model("commit");
 
 module.exports = app => {
-	app.get("/api/track/:date", (req, res) => {
-		console.log(req.params.date);
+	app.get("/api/track/", (req, res) => {
 		Commit.find({}).then(data => {
-			console.log(data);
 			res.send(data);
 		});
 	});
