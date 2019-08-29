6073fbd27d2800032fae841d04b5911f5aa4d371
Matt Bear <hi@matt.am>
2019-08-29 11:21:32 +1000

    create courselog collection schema

--- models/Courselog.js
+++ models/Courselog.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const devlogSchema = new Schema({
+const courselogSchema = new Schema({
 	duration: Number,
 	timestamp: Number,
 });
 
-mongoose.model("devlog", devlogSchema);
+mongoose.model("courselog", courselogSchema);
