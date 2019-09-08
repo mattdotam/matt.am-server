a8abd7c84cb6c57b7e7f8f77145b0d2424469a15
Matt Bear <hi@matt.am>
2019-09-08 18:22:15 +1000

    add POST and DELETE endpoints and status codes to measure API

--- routes/measureRoutes.js
+++ routes/measureRoutes.js
@@ -4,9 +4,18 @@ const requireLogin = require("../middlewares/requireLogin");
 const Measure = mongoose.model("measure");
 
 module.exports = app => {
+	app.get("/api/measures/", (req, res) => {
+		Measure.find({}).then(data => {
+			let results = [];
+			data.forEach(r =>
+				results.push({ id: r.id, timestamp: r.timestamp })
+			);
+			res.status(200).send(results);
+		});
+	});
 	app.get("/api/measures/:id", (req, res) => {
 		Measure.find({ id: req.params.id }).then(data => {
-			res.send(data);
+			res.status(200).send(data);
 		});
 	});
 	app.post("/api/measures", requireLogin, (req, res) => {
@@ -31,12 +40,46 @@ module.exports = app => {
 			calfR: m.calfR,
 			mmol: m.mmol,
 		});
-
 		try {
-			measure.save();
+			measure.status(201).save();
 			res.send(measure.id);
 		} catch (err) {
 			res.status(422).send(err);
 		}
+	});
+	app.put("/api/measures", requireLogin, (req, res) => {
+		const m = req.body;
+		Measure.update(
+			{ id: m.id },
+			{
+				timestamp: m.timestamp,
+				height: m.height,
+				weight: m.weight,
+				neck: m.neck,
+				chest: m.chest,
+				waist: m.waist,
+				belly: m.belly,
+				hips: m.hips,
+				bicepL: m.bicepL,
+				bicepR: m.bicepR,
+				forearmL: m.forearmL,
+				forearmR: m.forearmR,
+				thighL: m.thighL,
+				thighR: m.thighR,
+				calfL: m.calfL,
+				calfR: m.calfR,
+				mmol: m.mmol,
+			}
+		).then(data => {
+			res.status(200).send(data);
+		});
+	});
+	app.delete("/api/measures/:id", requireLogin, (req, res) => {
+		const id = req.params.id;
+		Measure.deleteOne({ id }, err => {
+			res.send(err);
+		}).then(res => {
+			res.status(200).send(res);
+		});
 	});
 };
