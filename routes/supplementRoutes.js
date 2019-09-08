37e137896540cd66b0575f9b21ba314c07b09192
Matt Bear <hi@matt.am>
2019-09-08 18:31:07 +1000

    add POST and DELETE endpoints and status codes to supplement API

--- routes/supplementRoutes.js
+++ routes/supplementRoutes.js
@@ -4,9 +4,18 @@ const requireLogin = require("../middlewares/requireLogin");
 const Supplement = mongoose.model("supplement");
 
 module.exports = app => {
+	app.get("/api/supplements/", (req, res) => {
+		Supplement.find({}).then(data => {
+			let results = [];
+			data.forEach(r =>
+				results.push({ id: r.id, timestamp: r.timestamp })
+			);
+			res.status(200).send(results);
+		});
+	});
 	app.get("/api/supplements/:id", (req, res) => {
 		Supplement.find({ id: req.params.id }).then(data => {
-			res.send(data);
+			res.status(200).send(data);
 		});
 	});
 	app.post("/api/supplements", requireLogin, (req, res) => {
@@ -20,9 +29,26 @@ module.exports = app => {
 		});
 		try {
 			supplement.save();
-			res.send(supplement.id);
+			res.status(201).send(supplement.id);
 		} catch (err) {
 			res.status(422).send(err);
 		}
+	});
+	app.put("/api/supplements", requireLogin, (req, res) => {
+		const { id, timestamp, name, unit, quantity } = req.body;
+		Supplement.update(
+			{ id },
+			{ timestamp, name, unit, quantity }
+		).then(data => {
+			res.status(200).send(data);
+		});
+	});
+	app.delete("/api/supplements/:id", requireLogin, (req, res) => {
+		const id = req.params.id;
+		Supplement.deleteOne({ id }, err => {
+			res.send(err);
+		}).then(res => {
+			res.status(200).send(res);
+		});
 	});
 };
