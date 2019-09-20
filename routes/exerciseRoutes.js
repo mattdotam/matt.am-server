345906a19a777dc1bc73dc960b2f41574ffed117
Matt Bear <hi@matt.am>
2019-09-20 13:16:56 +1000

    change exercise GET endpoint to accept queries to receive workout history using that exercise

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -2,12 +2,60 @@ const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 
 const Exercise = mongoose.model("exercise");
+const Workout = mongoose.model("workout");
 
 module.exports = app => {
 	app.get("/api/exercises", (req, res) => {
 		Exercise.find({}).then(data => {
 			res.status(200).send(data);
 		});
+	});
+	app.get("/api/exercises/:query", requireLogin, async (req, res) => {
+		const id = req.params.query;
+		const {
+			action = "perform",
+			find = "past",
+			number = 1,
+			at = Math.floor(Date.now() / 1000),
+		} = req.query;
+
+		let results = [];
+		if (action === "perform") {
+			if (find === "past") {
+				results = await Workout.find({
+					$and: [
+						{
+							timestamp: {
+								$lt: Number(at),
+							},
+						},
+						{
+							"activities.id": { $eq: id },
+						},
+					],
+				})
+					.select({ timestamp: true, _id: false, activities: true })
+					.sort({ timestamp: 1 })
+					.limit(Number(number));
+			} else if (find === "future") {
+				results = await Workout.find({
+					$and: [
+						{
+							timestamp: {
+								$gt: Number(at),
+							},
+						},
+						{
+							"activities.id": { $eq: id },
+						},
+					],
+				})
+					.select({ timestamp: true, _id: false, activities: true })
+					.sort({ timestamp: -1 })
+					.limit(Number(number));
+			}
+		}
+		res.send(results);
 	});
 	app.post("/api/exercises", requireLogin, (req, res) => {
 		const { id, name, tags } = req.body;
